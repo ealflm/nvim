2cb45024901efc454d34ce1a5ffcdfbd4ac431c9
NamDP <ealflm@gmail.com>
2024-03-29 20:58:05 +0700

    WIP

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,23 @@ local map = vim.keymap.set
 map("x", "p", 'p:let @+=@0<CR>:let @"=@0<CR>', { desc = "Dont copy replaced text" })
 
 map("x", "<C-c>", '"+y', { desc = "Copy to system" })
+
+vim.keymap.set("n", "<A-\\>", function()
+  vim.ui.input({ prompt = "Do you want to commit all (Y/N): " }, function(input)
+    if input and ((input == "") or string.lower(input) == "y") then
+      vim.cmd('!git add . && git commit -m "WIP"')
+    else
+      vim.cmd("echo ''")
+    end
+  end)
+end, { desc = "Git fast commit" })
+
+vim.keymap.set("n", "<C-\\>", function()
+  vim.ui.input({ prompt = "Do you want to push all (Y/N): " }, function(input)
+    if input and ((input == "") or string.lower(input) == "y") then
+      vim.cmd('!git add . && git commit -m "WIP" && git push')
+    else
+      vim.cmd("echo ''")
+    end
+  end)
+end, { desc = "Git fast push" })
