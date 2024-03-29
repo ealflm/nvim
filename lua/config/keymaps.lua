4c6f298d5fa9a8c0066947d035d17a2e7fe4fa0c
NamDP <ealflm@gmail.com>
2024-03-29 20:28:04 +0700

    commit

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("x", "p", 'p:let @+=@0<CR>:let @"=@0<CR>', { desc = "Dont copy replaced text" })
+
+map("x", "<C-c>", '"+y', { desc = "Copy to system" })
