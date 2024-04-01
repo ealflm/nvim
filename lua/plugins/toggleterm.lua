01a9283dffda130cde4ae9bb473a92bb4675f757
NamDP <ealflm@gmail.com>
2024-04-01 09:56:05 +0700

    WIP

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -13,7 +13,7 @@ return {
       require("toggleterm").setup({
         size = function(term)
           if term.direction == "horizontal" then
-            return vim.o.lines * 0.4
+            return vim.o.lines * 0.3
           elseif term.direction == "vertical" then
             return vim.o.columns * 0.4
           end
