244dd9c8970d90e699058f449f28c6731d1e8345
NamDP <ealflm@gmail.com>
2024-04-01 09:09:39 +0700

    WIP

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,9 +2,18 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Avoid auto comment when enter
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "*" },
   callback = function()
     vim.cmd("setlocal formatoptions-=o")
+  end,
+})
+
+-- fix NestJs module filetype
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*module.ts",
+  callback = function()
+    vim.cmd("e ++ff=dos")
   end,
 })
